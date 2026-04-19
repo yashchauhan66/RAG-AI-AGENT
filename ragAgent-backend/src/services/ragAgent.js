a9672f8e2e1f4f7c52e63c53fb973a5359852341
Your Name <your.email@example.com>
2026-04-19 09:28:28 +0530

    remove radis

--- ragAgent-backend/src/services/ragAgent.js
+++ ragAgent-backend/src/services/ragAgent.js
@@ -1,7 +1,6 @@
 import { generateEmbedding } from "./embeddingService.js";
 import { buildPrompt } from "../prompts/promptBuilder.js";
 import { addMessage, getHistory } from "./chatHistoryService.js";
-import redisClient from "../redis/redis.js";
 import { runAgent } from "./agentService.js";
 import { rewriteQuery } from "./queryRewriteService.js";
 import { rerankDocuments } from "./rerankerService.js";
@@ -24,28 +23,7 @@ export async function ragAgent(sessionId, question, res) {
       return;
     }
 
-    const cacheKey = `rag:${question}`;
 
-    const cached = await redisClient.get(cacheKey);
-
-    if (cached) {
-
-      console.log("Cache Hit");
-
-      addMessage(sessionId, "user", question);
-      addMessage(sessionId, "assistant", cached);
-
-      if (!res.headersSent) {
-        res.setHeader("Content-Type", "text/plain");
-        res.setHeader("Transfer-Encoding", "chunked");
-      }
-      res.write(String(cached));
-      res.end();
-
-      return;
-    }
-
-    console.log("Cache Miss");
 
     addMessage(sessionId, "user", question);
 
@@ -65,7 +43,7 @@ export async function ragAgent(sessionId, question, res) {
 
     addMessage(sessionId, "assistant", answer);
 
-    await redisClient.set(cacheKey, answer, { EX: 3600 });
+
 
   } catch (error) {
 
